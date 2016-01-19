6ced51a21eb0e012d545112602d843fef0c8de03
Roger Chen <rogerthechen@gmail.com>
2016-01-18 23:39:25 -0800

    Enable CORS and configure origins with coursecycle.yml

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module CoursecycleServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins Rails.application.config_for(:coursecycle)["origins"]
+        resource "*", :headers => :any, :methods => [:get]
+      end
+    end
   end
 end
